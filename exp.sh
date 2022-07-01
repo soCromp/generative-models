13c0c3bc1676b182531a3009a62b9f8113b380dc
soCromp <snc40@pitt.edu>
2022-07-01 10:49:34 -0500

    sync conf files

--- exp.sh
+++ exp.sh
@@ -1,8 +1,14 @@
-python run.py -m conf/model/exp/diffnone.yaml -d conf/data/mnist20k.yaml
-python run.py -m conf/model/exp/diffrandom10+10.yaml -d conf/data/mnist10k.yaml
-python run.py -m conf/model/exp/diffrarest10+10.yaml -d conf/data/mnist10k.yaml
-python run.py -m conf/model/exp/diffloss10+10.yaml -d conf/data/mnist10k.yaml
-python run.py -m conf/model/exp/diffrandom15+5.yaml -d conf/data/mnist15k.yaml
-python run.py -m conf/model/exp/diffrarest15+5.yaml -d conf/data/mnist15k.yaml
-python run.py -m conf/model/exp/diffloss15+5.yaml -d conf/data/mnist15k.yaml
-
+python train.py -m conf/model/exp/vaenone.yaml -d conf/data/mnist20k.yaml
+python train.py -m conf/model/exp/vaerandom15+5.yaml -d conf/data/mnist15k.yaml
+python train.py -m conf/model/exp/vaerarest15+5.yaml -d conf/data/mnist15k.yaml
+python train.py -m conf/model/exp/vaeloss15+5.yaml -d conf/data/mnist15k.yaml
+python train.py -m conf/model/exp/vaerandom10+10.yaml -d conf/data/mnist10k.yaml
+python train.py -m conf/model/exp/vaerarest10+10.yaml -d conf/data/mnist10k.yaml
+python train.py -m conf/model/exp/vaeloss10+10.yaml -d conf/data/mnist10k.yaml
+python train.py -m conf/model/exp/vaenone.yaml -d conf/data/celeba20k.yaml
+python train.py -m conf/model/exp/vaerandom15+5.yaml -d conf/data/celeba15k.yaml
+python train.py -m conf/model/exp/vaerarest15+5.yaml -d conf/data/celeba15k.yaml
+python train.py -m conf/model/exp/vaeloss15+5.yaml -d conf/data/celeba15k.yaml
+python train.py -m conf/model/exp/vaerandom10+10.yaml -d conf/data/celeba10k.yaml
+python train.py -m conf/model/exp/vaerarest10+10.yaml -d conf/data/celeba10k.yaml
+python train.py -m conf/model/exp/vaeloss10+10.yaml -d conf/data/celeba10k.yaml
